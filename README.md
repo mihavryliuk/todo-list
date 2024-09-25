a4e5f3670ea99b6a3a25c601e31001ddf60f7be2
Misha Havryliuk <misharob31415@gmail.com>
2024-09-25 14:45:57 +0300

    some edit, push

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# todo-list" 
+App for task manager
--- todo_list/settings.py
+++ todo_list/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo'
+    'todo',
+    'crispy_forms',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -112,6 +114,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 # Static files (CSS, JavaScript, Images)
