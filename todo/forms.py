5132e5544c55b68685669ef3bce76aea1491ab82
Misha Havryliuk <misharob31415@gmail.com>
2024-09-25 12:00:07 +0300

    forms

---
+++ todo/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from todo.models import Task, Tag
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ["content", "deadline", "is_done", "tags"]
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = ["name"]
